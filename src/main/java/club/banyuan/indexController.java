53a2f06b2987db9f8bceeb8fd19eb0e3f461a35f
jay958718389@qq.com <Tj199602>
2020-01-13 13:06:19 +0800

    init

--- src/main/java/club/banyuan/indexController.java
+++ src/main/java/club/banyuan/indexController.java
@@ -2,9 +2,15 @@ package club.banyuan;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class indexController {
-    @GetMapping("/{username}")
-    
+    @GetMapping("/{username:[a-z0-9_]+}")
+    @ResponseBody
+    String getUserBlog(@PathVariable String username){
+            return String.format("user %s",username);
+    }
+
 }
